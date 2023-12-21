4267dc4776b001fb457ebbdefea5337e9bbc7d52
JO-HYEWON <107868634+JO-HYEWON@users.noreply.github.com>
2023-12-22 03:29:55 +0900

    12.22 커밋
    / array, object state 변경하는법 까지

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-// npm start
+// npm startE
 
 // JSX 문법 3개
 // class는 className
@@ -12,14 +13,52 @@ function App() {
 
 let post = '강남 우동 맛집';
 
-  return (
+let [글제목1,b] = useState('남자 코트 추천');
+
+// 이렇게 쓰면 a라고 가져오면 '남자 코트 추천'이 나온다.
+// b는 state변경을 도와주는 함수임
+// 변수와 state의 차이점? - 일반 변수는 갑자기 변경되면 다시 html이 변경되어야 하지만
+// state는 아님. 갑자기 변경되면 자동으로 html이 랜더링이 됨.
+
+
+let[글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+let[날짜, d] = useState('2월 17일 발행');
+// return () 안에는 병렬로 태그 2개 이상 기입 금지
+// <div></div> 를 쓰고 그 다음 또 <div></div> 쓰면 안되고 1개만 써야함
+
+let [따봉, 따봉변경] = useState(0);
+
+return (
     <div className="App">
       <div className="black-nav">
         <h4 style={{color : 'red', fontSize : '16px'}}>블로그임</h4>
       </div>
-      <h4>{post}</h4>
+      <div className='list'>
+        <h4>{글제목[0] } <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} </h4>
+        <p>{날짜}</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>{날짜}</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>{날짜}</p>
+      </div>
+      <div>
+        <button onClick={() => {
+          let copy = [...글제목];
+          copy[0] = '여자 코트 추천';
+          글제목변경(copy);
+          }}>😜</button>
+          <button onClick={() => {
+            let copy = [...글제목];
+            글제목변경(copy.sort());
+          }}>가나다순정렬</button>
+      </div>
     </div>
   );
 }
+
 
 export default App;
